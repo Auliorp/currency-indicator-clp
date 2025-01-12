705003f835af6c3c310a3e75494669215fe6f022
Aulio Rovero <aulioroveroparedes@gmail.com>
2025-01-12 20:33:18 -0300

    wip: added angular/material and working in components inside file shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { SharedModule } from './shared/shared.module'; 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    CommonModule,
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
